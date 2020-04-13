ff63ac6c2b30011c74646994dfa681789206d5c7
Mark Cascardi <mcascardi@gmail.com>
2020-04-13 04:44:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 
 document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  } else if (e.which === 39) {
+  // if (e.which === 37) {
+  //   moveDodgerLeft()
+   if (e.which === 39) {
      moveDodgerRight()
   }
 })
 
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
-  }
-}
+// function moveDodgerLeft() {
+//   var leftNumbers = dodger.style.left.replace('px', '')
+//   var left = parseInt(leftNumbers, 10)
+//
+//   if (left > 0) {
+//     dodger.style.left = `${left - 1}px`
+//   }
+// }
 
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
