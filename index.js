7e35e602a3d9c21c7a6b81ebe6274364225659c3
Mark Cascardi <mcascardi@gmail.com>
2020-04-13 04:42:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ var dodger = document.getElementById('dodger')
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
-    // else if (e.which === 39) {
-    //   moveDodgerRight()
-    // }
+  } else if (e.which === 39) {
+     moveDodgerRight()
   }
 })
 
@@ -19,11 +18,11 @@ function moveDodgerLeft() {
   }
 }
 
-// function moveDodgerRight() {
-//   var rightNumbers = dodger.style.right.replace('px', '')
-//   var right = parseInt(rightNumbers, 10)
-//
-//   if (right > 0) {
-//     dodger.style.left = `${right - 1}px`
-//   }
-// }
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right < 360) {
+    dodger.style.right = `${right + 1}px`
+  }
+}
