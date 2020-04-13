520bfde6b76d15a871ee00a23734925a00ce84ce
Mark Cascardi <mcascardi@gmail.com>
2020-04-13 04:56:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   debugger
-  var rightNumbers = dodger.style.left.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
 
   if (left < 360) {
     dodger.style.left = `${left + 1}px`
